d9bf3eadfefcbc6f3ac96df7ac42cba5dfc81126
Haruka <art.art.art861@gmail.com>
2026-01-25 15:38:43 +0900

    リポジトリ追加

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
@@ -56,9 +56,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </trpc.Provider>
 );
